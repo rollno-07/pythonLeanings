5b89c0e93570cac00efbd8e116fb14e3b08d4205
Vivek Rawat <vivekrawat0107@gmail.com>
2025-03-26 16:38:42 +0530

    started learning list

---
+++ pythonList.py
@@ -0,0 +1,12 @@
+# Python Lists – The Power of Ordered Collections
+
+# What are Lists in Python?
+
+# Lists are ordered, mutable collections that can hold multiple items in a single variable.
+
+# Creating, Accessing, and Modifying Lists
+
+my_list = [1, 2, 3, 4, 5]
+print(my_list[0])  # Accessing first element
+my_list[2] = 99    # Modifying an element
+print(my_list)
