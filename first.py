8755653718c01bc1d97c8bf53a5e134dcce92a6d
Vivek Rawat <vivekrawat0107@gmail.com>
2025-03-26 15:09:42 +0530

    print() function

--- first.py
+++ first.py
@@ -1,1 +1,27 @@
+#1. Printing Output Use the print() function to display output
+#The print() function is used to display text or other data on the screen. It is one of the most common and essential functions in Python.
+
 print("Hello, World!")
+
+
+#rinting Multiple Values
+name="Alice"
+age=25
+print("Name:",name,"Age:",age)
+
+# Using f-strings for Formatting
+print(f"Name: {name}, Age: {age}")
+
+# Adding Newlines and Special Characters
+print("Line 1\nLine 2\nLine 3")
+print("Hello\tWorld") # Tab space
+
+
+# Controlling End Parameter
+print("Hello", end=" ")
+print("World!")
+
+# Customizing Separators
+print("Python", "is", "awesome", sep=" - ")
+
+
